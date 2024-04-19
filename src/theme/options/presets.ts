0b2c22caa48315a1a7de37da7c0779e8f17a6038
kntskothiya <knts.kothiya@gmail.com>
2024-04-19 17:59:24 +0530

    ss

--- src/theme/options/presets.ts
+++ src/theme/options/presets.ts
@@ -1,10 +1,10 @@
 import { alpha } from '@mui/material/styles';
 
-import { grey, primary } from '../palette';
+import { primary } from '../palette';
 
 // ----------------------------------------------------------------------
 
-type PresetType = 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red';
+type PresetType = 'default' | 'purple';
 
 export function createPresets(preset: PresetType) {
   const primaryColor = getPrimary(preset);
@@ -25,15 +25,6 @@ export function createPresets(preset: PresetType) {
 
 // ----------------------------------------------------------------------
 
-const cyan = {
-  lighter: '#CCF4FE',
-  light: '#68CDF9',
-  main: '#078DEE',
-  dark: '#0351AB',
-  darker: '#012972',
-  contrastText: '#FFFFFF',
-};
-
 const purple = {
   lighter: '#EBD6FD',
   light: '#B985F4',
@@ -43,49 +34,14 @@ const purple = {
   contrastText: '#FFFFFF',
 };
 
-const blue = {
-  lighter: '#D1E9FC',
-  light: '#76B0F1',
-  main: '#2065D1',
-  dark: '#103996',
-  darker: '#061B64',
-  contrastText: '#FFFFFF',
-};
-
-const orange = {
-  lighter: '#FEF4D4',
-  light: '#FED680',
-  main: '#fda92d',
-  dark: '#B66816',
-  darker: '#793908',
-  contrastText: grey[800],
-};
-
-const red = {
-  lighter: '#FFE3D5',
-  light: '#FFC1AC',
-  main: '#FF3030',
-  dark: '#B71833',
-  darker: '#7A0930',
-  contrastText: '#FFFFFF',
-};
-
 export const presetOptions = [
   { name: 'default', value: primary.main },
-  { name: 'cyan', value: cyan.main },
   { name: 'purple', value: purple.main },
-  { name: 'blue', value: blue.main },
-  { name: 'orange', value: orange.main },
-  { name: 'red', value: red.main },
 ];
 
 export function getPrimary(preset: PresetType) {
   return {
-    default: primary,
-    cyan,
+    default: purple,
     purple,
-    blue,
-    orange,
-    red,
   }[preset];
 }
